27f6e635d532ab2625db790e928cc5de1952a259
noobys <som3dia@gmail.com>
2014-11-18 15:45:05 +0800

    Added Bootstrap

--- application/views/register_view.php
+++ application/views/register_view.php
@@ -1,9 +1,14 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<title>Register</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Register</title>
+	<link href="<?php base_url(); ?>css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
+<div class="container">
 	<?php echo validation_errors('<p class="error">');?>
 	<?php echo form_open("register/adduser");?>
 	<p>
@@ -30,5 +35,10 @@
 	<input type="submit" value="REGISTER">
 	</p>
 	<?php echo form_close();?>
+</div>
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="<?php base_url(); ?>js/bootstrap.min.js"></script>
 </body>
 </html>
