1bfc712e767c3fb3c5ccefbc59cfb2d9824f983c
newbey <som3dia@gmail.com>
2014-11-19 22:35:01 +0800

    profile page update

--- application/views/profile_body.php
+++ application/views/profile_body.php
@@ -1,3 +1,4 @@
+<!--Top Navigation-->
 <div class="topnav">
 	<div class = "container">
 		<div class="row">
@@ -7,7 +8,7 @@
 					<div class="col-md-2">
 						<div class="fixed cropcenterimg">
 	  						<div class="thumb">
-	  							<img src="http://www.desktop4ipad.com/wp-content/uploads/2011/07/Landscape-Grass_O-500x500.jpg"/>
+	  							<img src="<?php echo base_url();?>images/admin/profile.jpg"/>
 	  						</div>
 						</div>
 					</div>
@@ -16,6 +17,48 @@
 					</div>
 				</div>
 			</div>
-		</div>
+		</div>	
+</div>
+</div>
+<!--Main Body-->
+<div class="container">
+	<div class="wrapper">
+			<div class="row">
+				<div class="col-md-4">
+						<div class="bio">
+							<div class="row">
+								<div class="col-md-4">
+									<div class="fixed cropcenterimg">
+				  						<div class="profile_pic">
+				  								<img src="<?php echo base_url();?>images/admin/profile.jpg"/>
+				  						</div>
+									</div>
+								</div>
+								<div class="col-md-8">
+									<?php echo $this->session->userdata('username'); ?>
+									<br> 
+									<span class="glyphicon glyphicon-map-marker" aria-hidden="true"></span>Cebu City
+								</div>
+							</div>
+							<div class="row">
+								<div class="minidash">
+									<ul class="list-group">
+									  <li class="list-group-item">
+									    <span class="badge">20</span>
+									    Places Traveled
+									  </li>
+									  <li class="list-group-item">
+									    <span class="badge">14</span>
+									    Destination List
+									  </li>
+									</ul>
+								</div>
+							</div>
+						</div>
+				</div>
+				<div class="col-md-8">
+						<div class="feed">Profile Here</div>
+				</div>
+			</div>
 	</div>
 </div>
--- application/views/profile_header.php
+++ application/views/profile_header.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title><?php echo (isset($title)) ? $title : "BulkMark" ?> </title>
+	<title><?php echo (isset($title)) ? $title : "Places To Travel" ?> </title>
 	<link href="<?php echo base_url(); ?>css/bootstrap.min.css" rel="stylesheet">
 	<link href="<?php echo base_url(); ?>css/style.css" rel="stylesheet">
 
