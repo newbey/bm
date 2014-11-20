b663b28ec0f612ae020dcb1e1d84c13e5db8c587
noobys <som3dia@gmail.com>
2014-11-20 16:05:20 +0800

    updated profile body

--- application/views/profile_body.php
+++ application/views/profile_body.php
@@ -2,7 +2,7 @@
 <div class="topnav">
 	<div class = "container">
 		<div class="row">
-			<div class="col-md-4">THIS IS THE LOGO</div>
+			<div class="col-md-4">PLACES TO TRAVEL</div>
 			<div class="col-md-8">
 				<div class="navigation">
 					<div class="col-md-2">
@@ -35,29 +35,51 @@
 									</div>
 								</div>
 								<div class="col-md-8">
-									<?php echo $this->session->userdata('username'); ?>
-									<br> 
-									<span class="glyphicon glyphicon-map-marker" aria-hidden="true"></span>Cebu City
+									<div class="pubname col-md-12">
+										<?php echo $this->session->userdata('username'); ?>
+									</div>
+								
+									<div class="username col-md-12">
+										<?php echo '@'. $this->session->userdata('username'); ?>
+									</div>
 								</div>
 							</div>
 							<div class="row">
 								<div class="minidash">
-									<ul class="list-group">
-									  <li class="list-group-item">
-									    <span class="badge">20</span>
-									    Places Traveled
-									  </li>
-									  <li class="list-group-item">
-									    <span class="badge">14</span>
-									    Destination List
-									  </li>
-									</ul>
+									<div class="userbio"><span class="glyphicon glyphicon-map-marker" aria-hidden="true" style="padding-right:10px"></span>Cebu City</div>
+									<div class="userbio"><span class="glyphicon glyphicon-globe" aria-hidden="true" style="padding-right:10px"></span>www.yoursite.com</div>
+									<div class="userbio"><span class="glyphicon glyphicon-phone-alt" aria-hidden="true" style="padding-right:10px"></span>09123456</div>
+									<button type="button" class="btn btn-primary col-md-12">ADD TRAVEL BUDDY</button><br><br>
+									<button type="button" class="btn btn-success col-md-12">RECOMMENDATIONS</button>
 								</div>
 							</div>
 						</div>
 				</div>
 				<div class="col-md-8">
-						<div class="feed">Profile Here</div>
+						<div class="feed">
+							<div class="row">
+								<div class="col-md-3 ">
+									<div class="stats"><h2>12</h2>
+									<br>
+									<p><span class="glyphicon glyphicon-globe" aria-hidden="true"></span><span style="margin-left:8px"/>Places Visited</p></div>
+								</div>
+								<div class="col-md-3">
+									<div class="stats"><h2>4</h2>
+									<br>
+									<p><span class="glyphicon glyphicon-plane" aria-hidden="true"></span><span style="margin-left:8px"/>Destinations</p></div>
+								</div>
+								<div class="col-md-3">
+									<div class="stats"><h2>100</h2>
+									<br>
+									<p><span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span><span style="margin-left:8px"/>Followers</p></div>
+								</div>
+								<div class="col-md-3">
+									<div class="stats"><h2>12</h2>
+									<br>
+									<p><span class="glyphicon glyphicon-heart-empty" aria-hidden="true"></span><span style="margin-left:8px"/>Travel Buddies</p></div>
+								</div>
+							</div>
+						</div>
 				</div>
 			</div>
 	</div>
